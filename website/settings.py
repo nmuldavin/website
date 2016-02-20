54a7700f7bf5e8d18ca6242b00064cb7863bba65
nmuldavin <nmuldavin@gmail.com>
2016-02-20 02:09:30 -0800

    shit

--- website/settings.py
+++ website/settings.py
@@ -77,7 +77,3 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
